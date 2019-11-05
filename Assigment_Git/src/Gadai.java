3f3a5530489e47768fc6437b3ea52b83b7b6880b
yunitaasiagian22 <57378771+yunitaasiagian22@users.noreply.github.com>
2019-11-05 14:42:40 +0700

    Gadai 1

--- Assigment_Git/src/Gadai.java
+++ Assigment_Git/src/Gadai.java
@@ -1,4 +1,20 @@
 
 public class Gadai {
+	private String nama;
+	private String category;
+	private String description;
+	private int price;
+	
+	Gadai(String nama, String cateogory, String description, int price){
+		this.nama=nama;
+		this.category=category;
+		this.description=description;
+		this.price=price;
+	}
+	
+	
+	
+	
+	
 
 }
