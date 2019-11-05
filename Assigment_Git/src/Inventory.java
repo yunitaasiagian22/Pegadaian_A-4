042856b8eb4b9d11fb89b01c82a253ecb8d78b60
yunitaasiagian22 <57378771+yunitaasiagian22@users.noreply.github.com>
2019-11-05 15:15:18 +0700

    inventory

--- Assigment_Git/src/Inventory.java
+++ Assigment_Git/src/Inventory.java
@@ -1,17 +1,21 @@
 
 public class Inventory {
-	private String Nama, Product, Description;
-	private int ID, Price;
+	private String nama, product, description;
+	private int id, price;
 	public Inventory (String nama, String product, String description, int id, int price) {
 			super ();
-			Nama = nama;
-			Product = product;
-			Description = description;
-			ID = id;
-			Price = price;
+			nama = nama;
+			product = product;
+			description = description;
+			id = id;
+			price = price;
+	}
+	
+	public void setId (int id){
+		this.id=id;	
 	}
 	
 	
 	
-	}
+	
 }
