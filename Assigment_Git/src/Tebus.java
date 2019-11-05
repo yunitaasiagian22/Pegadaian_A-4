286ee07b594621837fa7d61e7575d476458d5620
dandles <dandles.banjarnahor@gmail.com>
2019-11-05 17:37:13 +0700

    1.4 Fixed class
    Tolong dicek ulang, kalau sudah bisa lanjut ke functionnya ya

--- Assigment_Git/src/Tebus.java
+++ Assigment_Git/src/Tebus.java
@@ -1,4 +1,5 @@
 
+
 public class Tebus {
 	private int ID;
 	private String Product;
@@ -6,68 +7,61 @@ public class Tebus {
 	private String Status;
 	private int Utang;
 	private int HargaBayar;
+	private String Category;
 	
 	
-	Tebus (int ID;
-	String Product;
-	int Price;
-	String Status;
-	int Utang;
-	int HargaBayar;
-	Tebus (int ID,
-	String Product,
-	int Price,
-	String Status,
-	int Utang,
-	int HargaBayar) {
+	Tebus (int ID, String Product, int Price, String Status, int Utang, int HargaBayar) {
 		super();
 			this.ID=ID;
 			this.Product=Product;
 			this.Price=Price;
-			this.Status=status;
+			this.Status=Status;
 			this.Utang=Utang;
 			this.HargaBayar=HargaBayar;
 			}
 			
-	public String getTebusInfo1(){
-		return "ID:  " + id + "\nProduct:  " + Product + "\nPrice:  " + price + "\nStatus: "+ status+"\nUtang: "+ utang+"\nHarga Bayar: "+ Bayar+"\n";
-			}
-	public String getTebusInfo(){
-		return "ID:  " + id + "\nProduct:  " + Product + "\nPrice:  " + price + "\nStatus: "+ status+"\nUtang: "+ utang+"\nHarga Bayar: "+ Bayar+"\n";
-		public int getID() {
-			return id;
-		}
-		
-	public void setID(int id) {
-		this.id = id;
-			
-	public String Product() {
-	return Product;
-	public void setProduct(String Product) {
-		this.Product = Product;
-				
-	public  int Price() {
-	return Price;
-	public void setPrice( int Price) {
-		this.Price = Price;
-	public String getStatus() {
-	return Status;
-	public void setStatus(String Status) {
-	this.Status = Status;
-	public  int Utang() {
-	return Utang;
-	public void setUtang( int Utang) {
-	this.Utang = Utang;
-	public  int HargaBayar() {
-	return HargaBayar;
-	public void setBayar( int Bayar) {
-	this.Bayar = Bayar;
+	public int getHargabayar() {
+		return HargaBayar;
+	}
+
+	public void setHargabayar(int hargaBayar) {
+		HargaBayar = hargaBayar;
+	}
+
+	
+
+	public int getHargaBayar() {
+		return HargaBayar;
+	}
+
+	public void setHargaBayar(int hargaBayar) {
+		HargaBayar = hargaBayar;
+	}
+
+	public String getcategory() {
+		return Category;
+	}
+
+	public void setCategory(String category) {
+		category = Category;
+	}
+
+	public String getProduct() {
+		return Product;
+	}
+
+	public int getPrice() {
+		return Price;
+	}
+
+	public int getUtang() {
+		return Utang;
 	}
 	
-	Scanner input = new Scanner(System.in); //input
-	System.out.println("Masukkan ID barang yang ingin di tebus: ");
-	int ID = input.nextInt() ;
-	System.out.println("Masukkan biaya yang ingin di bayarkan: ");
-	int HargaBayar = input.nextInt() ;
+}
+
+	
+	
+
 	
 	
