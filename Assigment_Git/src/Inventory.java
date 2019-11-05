2c416caad3e671b28d4f1be5f771bad2d1b099de
unknown <iman.widyatama@pegadaian.co.id>
2019-11-05 15:02:37 +0700

    Inventory

--- Assigment_Git/src/Inventory.java
+++ Assigment_Git/src/Inventory.java
@@ -1,4 +1,17 @@
 
 public class Inventory {
-
+	private String Nama, Product, Description;
+	private int ID, Price;
+	public Inventory (String nama, String product, String description, int id, int price) {
+			super ();
+			Nama = nama;
+			Product = product;
+			Description = description;
+			ID = id;
+			Price = price;
+	}
+	
+	
+	
+	}
 }
